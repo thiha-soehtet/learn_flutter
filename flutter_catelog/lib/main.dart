5d72fa8e001a8d6a882d32a79b7eddbc0614df77
thiha-soehtet <contact.thihasoehtet@gmail.com>
2021-01-06 09:34:28 +0630

    add lists

--- flutter_catelog/lib/main.dart
+++ flutter_catelog/lib/main.dart
@@ -50,68 +50,233 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Container(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            ExpansionTile(
+              title: Text(
+                "Widgets",
+                style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              children: <Widget>[
+                ListTile(
+                  title: Text(
+                      'Text'
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                      'Text Field'
+                  ),
+                )
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            ExpansionTile(
+              title: Text(
+                "Layouts",
+                style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              children: <Widget>[
+                ListTile(
+                  title: Text(
+                      'Text'
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                      'Text Field'
+                  ),
+                )
+              ],
+            ),
+            ExpansionTile(
+              title: Text(
+                "Lists",
+                style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              children: <Widget>[
+                ListTile(
+                  title: Text(
+                      'Text'
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                      'Text Field'
+                  ),
+                )
+              ],
+            ),
+            ExpansionTile(
+              title: Text(
+                "Appbar",
+                style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              children: <Widget>[
+                ListTile(
+                  title: Text(
+                      'Text'
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                      'Text Field'
+                  ),
+                )
+              ],
+            ),
+            ExpansionTile(
+              title: Text(
+                "Navigation",
+                style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              children: <Widget>[
+                ListTile(
+                  title: Text(
+                      'Text'
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                      'Text Field'
+                  ),
+                )
+              ],
+            ),
+            ExpansionTile(
+              title: Text(
+                "Animation",
+                style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              children: <Widget>[
+                ListTile(
+                  title: Text(
+                      'Text'
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                      'Text Field'
+                  ),
+                )
+              ],
+            ),
+            ExpansionTile(
+              title: Text(
+                "Multimedia",
+                style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              children: <Widget>[
+                ListTile(
+                  title: Text(
+                      'Text'
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                      'Text Field'
+                  ),
+                )
+              ],
+            ),
+            ExpansionTile(
+              title: Text(
+                "Local Database",
+                style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              children: <Widget>[
+                ListTile(
+                  title: Text(
+                      'Text'
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                      'Text Field'
+                  ),
+                )
+              ],
+            ),
+            ExpansionTile(
+              title: Text(
+                "Charts",
+                style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              children: <Widget>[
+                ListTile(
+                  title: Text(
+                      'Text'
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                      'Text Field'
+                  ),
+                )
+              ],
+            ),
+            ExpansionTile(
+              title: Text(
+                "Networking",
+                style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              children: <Widget>[
+                ListTile(
+                  title: Text(
+                      'Text'
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                      'Text Field'
+                  ),
+                )
+              ],
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      )
     );
   }
 }
